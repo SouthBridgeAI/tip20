6fc7fe7af29eafef6dee0a66cd5ef298dc590d9c
Hrishi Olickel <hrishioa@gmail.com>
2024-10-04 16:45:03 -0700

    Fixing module issues

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  entry: ["src/index.ts"],
   target: "es2020",
   format: ["cjs", "esm"],
   splitting: false,
   sourcemap: true,
   clean: true,
   dts: true,
+  outDir: "dist",
+  outExtension({ format }) {
+    return {
+      js: format === "cjs" ? ".cjs" : ".js",
+    };
+  },
 });
